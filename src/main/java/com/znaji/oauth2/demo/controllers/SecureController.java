85ce7450910d23496cbda378682de2bd633bc329
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-09-27 19:09:56 +0100

    #3 configure our app as an oAuth2 resource server

--- src/main/java/com/znaji/oauth2/demo/controllers/SecureController.java
+++ src/main/java/com/znaji/oauth2/demo/controllers/SecureController.java
@@ -1,5 +1,10 @@
 package com.znaji.oauth2.demo.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,7 +12,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SecureController {
 
     @GetMapping("/secure")
-    public String secure() {
+    public String secure(Authentication authentication) {
+        if (authentication instanceof UsernamePasswordAuthenticationToken un) {
+            System.out.println(un);
+        } else if (authentication instanceof OAuth2AuthenticationToken oa) {
+            System.out.println(oa);
+        }
         return "secure.html";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/secure-api")
+    public ResponseEntity<String> secureApi() {
+        return ResponseEntity.ok("Hello from /secure api endpoint");
     }
 }
